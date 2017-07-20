5df2c29f89c7b87fa4d5d783f0d3831bf32aec1b
Flavio Pace <flavio.pace@gmail.com>
2017-07-20 23:46:21 +0200

    Added layer switch

--- OpenLayer/Esempio_5/OL4_Esempio5.js
+++ OpenLayer/Esempio_5/OL4_Esempio5.js
@@ -33,32 +33,32 @@ function init() {
         // componente HTML dove posizionare la Mapps
         target: 'map',
         layers: [
-          new ol.layer.Tile({
-            source: new ol.source.TileJSON({
-              url: 'https://api.tiles.mapbox.com/v3/mapbox.geography-class.json?secure',
-              crossOrigin: 'anonymous'
-            })
-        }),
-        lyr_confinicomunali,
-        lyr_centrilocalita
+              new ol.layer.Tile({
+                  // Usa un Layer di Tipo Tile da OpenStreetMap
+                  source: new ol.source.OSM()
+              }),
+              lyr_centrilocalita,
+              lyr_confinicomunali,
         ],
-        controls: [
-            //Define the default controls
-            new ol.control.Zoom(),
-            new ol.control.Rotate(),
-            new ol.control.Attribution(),
-            //Define some new controls
-            //new ol.control.ZoomSlider(),
-            //new ol.control.MousePosition(),
-            //new ol.control.ScaleLine(),
-            //new ol.control.OverviewMap()
-        ],
+        controls: ol.control.defaults({
+          attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+              collapsible: false
+              })
+            }).extend([
+              new ol.control.ScaleLine({}),
+              new ol.control.LayerSwitcher({tipLabel: "Layers"}),
+          ]),
         // Posizionamento della View coordinate Potenza
         view: new ol.View({
             center: ol.proj.transform([15.8022214, 40.6372425], 'EPSG:4326', 'EPSG:3857'),
             zoom: 7
         })
     });
+
+    var layerSwitcher = new ol.control.LayerSwitcher({
+        tipLabel: 'Légende' // Optional label for button
+    });
+    map.addControl(layerSwitcher);
 }
 // Questo ci assicura che lo script JavaScript viene eseguito solo
 // Quando la DOM e' stata completamente caricata
