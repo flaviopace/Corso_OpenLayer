fea5bf8bb164011766bd0b356a655ef5262823d1
Flavio Pace <flavio.pace@gmail.com>
2017-07-20 23:00:26 +0200

    Aggiunto Esempio 4, confini comunali

---
+++ OpenLayer/Esempio_4/OL4_Esempio4.js
@@ -0,0 +1,50 @@
+function init() {
+    document.removeEventListener('DOMContentLoaded', init);
+
+    var format_confinicomunali = new ol.format.GeoJSON();
+    var features_confinicomunali = format_confinicomunali.readFeatures(geojson_confinicomunali,
+                                { dataProjection: 'EPSG:4326',
+                                 featureProjection: 'EPSG:3857'
+                                });
+    var jsonSource_confinicomunali = new ol.source.Vector();
+    jsonSource_confinicomunali.addFeatures(features_confinicomunali);
+
+    var lyr_confinicomunali = new ol.layer.Vector({
+            source:jsonSource_confinicomunali,
+            title: "Confini Comunali",
+            name: "confini_comunali"
+    });
+
+    var map = new ol.Map({
+        // componente HTML dove posizionare la Mapps
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.TileJSON({
+              url: 'https://api.tiles.mapbox.com/v3/mapbox.geography-class.json?secure',
+              crossOrigin: 'anonymous'
+            })
+        }),
+        lyr_confinicomunali
+        ],
+        controls: [
+            //Define the default controls
+            new ol.control.Zoom(),
+            new ol.control.Rotate(),
+            new ol.control.Attribution(),
+            //Define some new controls
+            //new ol.control.ZoomSlider(),
+            //new ol.control.MousePosition(),
+            //new ol.control.ScaleLine(),
+            //new ol.control.OverviewMap()
+        ],
+        // Posizionamento della View coordinate Potenza
+        view: new ol.View({
+            center: ol.proj.transform([15.8022214, 40.6372425], 'EPSG:4326', 'EPSG:3857'),
+            zoom: 7
+        })
+    });
+}
+// Questo ci assicura che lo script JavaScript viene eseguito solo
+// Quando la DOM e' stata completamente caricata
+document.addEventListener('DOMContentLoaded', init);
