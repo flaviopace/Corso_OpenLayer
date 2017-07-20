09f89189856712310acc6c45d182fe860d2623ad
Flavio Pace <flavio.pace@gmail.com>
2017-07-20 20:52:06 +0200

    Aggiunto layer

--- OpenLayer/Esempio_1/OL4_Esempio1.js
+++ OpenLayer/Esempio_1/OL4_Esempio1.js
@@ -2,9 +2,11 @@ function init() {
     document.removeEventListener('DOMContentLoaded', init);
 
     var map = new ol.Map({
+        // componente HTML dove posizionare la Mapps
         target: 'map',
         layers: [
             new ol.layer.Tile({
+                // Usa un Layer di Tipo Tile da OpenStreetMap
                 source: new ol.source.OSM()
             })
         ],
@@ -19,10 +21,13 @@ function init() {
             //new ol.control.ScaleLine(),
             //new ol.control.OverviewMap()
         ],
+        // Posizionamento della View
         view: new ol.View({
             center: [0, 0],
             zoom: 2
         })
     });
 }
+// Questo ci assicura che lo script JavaScript viene eseguito solo
+// Quando la DOM e' stata completamente caricata
 document.addEventListener('DOMContentLoaded', init);
